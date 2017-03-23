57895eeb988ada5a07c5610b0ac1de0796908905
chen <chen@MYERS-PC-18>
2017-03-23 12:13:23 +0100

    Code cleanup

--- DscopeVS/Source.c
+++ DscopeVS/Source.c
@@ -3,10 +3,8 @@
 #include <limits.h>
 #include "NiFpga_FPGA.h"
 
-
-
-#define FIFODEPTH1 1 //number of elements in the FIFO1
-#define FIFODEPTH2 1
+#define AOFIFODEPTH 1 //number of elements in the AO FIFO
+#define DIOFIFODEPTH 1 //number of elements in the DIO FIFO
 
 /*Define the full path of the bitfile*/
 static const char* const Bitfile = "D:\\OwnCloud\\Codes\\Dscope\\DscopeVS\\LabView\\FPGA Bitfiles\\" NiFpga_FPGA_Bitfile;
@@ -47,30 +45,33 @@ int main(void)
 			uint32_t freq = 1000000;
 			uint32_t AO0rate = 1; //in us
 			uint32_t timeout = 10000/* 10 seconds */;
-			size_t r1, r2;
 			NiFpga_MergeStatus(&status, NiFpga_WriteU32(session, NiFpga_FPGA_ControlU32_freq, freq)); //rate
 			NiFpga_MergeStatus(&status, NiFpga_WriteU32(session, NiFpga_FPGA_ControlU32_AOLoopPeriodus, AO0rate));
 
 			/*AO1 FIFO*/
-			uint32_t AO1rate = 1; //in us
-			size_t size = FIFODEPTH1;
-			int16_t AOFIFOdata[FIFODEPTH1]; //the analog output takes a signed 16-bit int
-			AOFIFOdata[0] = _I16_MAX;
-			NiFpga_MergeStatus(&status, NiFpga_WriteU32(session, NiFpga_FPGA_ControlU32_AO1LoopPeriodus, AO1rate)); //rate
-			NiFpga_MergeStatus(&status, NiFpga_WriteFifoU32(session, NiFpga_FPGA_HostToTargetFifoI16_A0FIFO, AOFIFOdata, size, timeout, &r1));
+			uint32_t AOfifoRate = 1; //in us
+			size_t r1;
+			size_t sizeAOfifo = AOFIFODEPTH;
+			int16_t AOfifo[AOFIFODEPTH]; //the analog output takes a signed 16-bit int
+			AOfifo[0] = _I16_MAX;
+			NiFpga_MergeStatus(&status, NiFpga_WriteU32(session, NiFpga_FPGA_ControlU32_AO1LoopPeriodus, AOfifoRate)); //rate
+			NiFpga_MergeStatus(&status, NiFpga_WriteFifoU32(session, NiFpga_FPGA_HostToTargetFifoI16_A0FIFO, AOfifo, sizeAOfifo, timeout, &r1));
 
 
 			/*DIO0*/
-			uint32_t DIOrate = 1;//in us
+			uint32_t DIOrate = 1; //in us
 			NiFpga_Bool DIO0 = 1;
 			NiFpga_MergeStatus(&status, NiFpga_WriteU32(session, NiFpga_FPGA_ControlU32_DIOLoopPeriodus, DIOrate)); //rate
 			NiFpga_MergeStatus(&status, NiFpga_WriteBool(session, NiFpga_FPGA_ControlBool_Connector0DIO0, DIO0));
-			/*FIFO DIO1*/
-			size_t sizeDIO = FIFODEPTH2;
-			NiFpga_Bool DOFIFOdata[FIFODEPTH2];
-			DOFIFOdata[0] = 1;
-			NiFpga_MergeStatus(&status, NiFpga_WriteU32(session, NiFpga_FPGA_ControlU32_DOIFIFOLoopPeriodus, DIOrate)); //rate
-			NiFpga_MergeStatus(&status, NiFpga_WriteFifoBool(session, NiFpga_FPGA_HostToTargetFifoBool_DIOFIFO, DOFIFOdata, sizeDIO, timeout, &r2));
+			
+			/*DIO FIFO*/
+			uint32_t DIOfifoRate = 1; //in us
+			size_t r2;
+			size_t sizeDIOfifo = DIOFIFODEPTH;
+			NiFpga_Bool DIOfifo[DIOFIFODEPTH];
+			DIOfifo[0] = 1;
+			NiFpga_MergeStatus(&status, NiFpga_WriteU32(session, NiFpga_FPGA_ControlU32_DOIFIFOLoopPeriodus, DIOfifoRate)); //rate
+			NiFpga_MergeStatus(&status, NiFpga_WriteFifoBool(session, NiFpga_FPGA_HostToTargetFifoBool_DIOFIFO, DIOfifo, sizeDIOfifo, timeout, &r2));
 
 
 

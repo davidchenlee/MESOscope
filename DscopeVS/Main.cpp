9bf8df4428b2e03021c960ea1439e45faa34754c
David Chen <chen@mpi-cbg.de>
2017-06-19 17:03:41 +0200

    Switched from C to C++ because the stages are implemented in C++

--- DscopeVS/Main.cpp
+++ DscopeVS/Main.cpp
@@ -1,29 +1,11 @@
-/*
-*Date: April 2017
-*Author: David Chen
-*Institution: Max Planck Institute for Cell Biology and Genetics
-*Description: Main program for controlling the DEEPscope via the NI 7852R card
-*Source: http://www.ni.com/tutorial/8638/en/ 
-*/
-
-
-/*
-2^16 * 25ns = 1.6384 ms
-2^32 * 25ns = 107.3741824 s
-
-maybe I shoud do he following package:
-line 1: 16 bits for the channel ID, 16 bits for the AO value
-line 2: 32 bits for the wait-time
-*/
-
-
-#include <stdio.h>
-#include <stdlib.h> //For malloc
-#include <math.h>
-#include <time.h>
+//#include <stdio.h>
+//#include <stdlib.h> //For malloc
 //#include <ansi_c.h>
 //#include <limits.h>
+//#include <math.h>
+#include <time.h>
 #include "NiFpga_FPGA.h"
+#include <iostream>
 #include "main.h"
 
 
@@ -37,7 +19,7 @@ static const char* const Bitfile = "D:\\OwnCloud\\Codes\\Dscope\\DscopeVS\\LabVi
 void delay(double dly) {
 	/* save start time */
 	const time_t start = time(NULL);
-
+	
 	time_t current;
 	do {
 		/* get current time */
@@ -48,14 +30,12 @@ void delay(double dly) {
 }
 
 
-
-int main(void)
+int main()
 {
 	/* must be called before any other FPGA calls */
 	NiFpga_Status status = NiFpga_Initialize();
 
-	printf("FPGA status: %d\n", status);
-
+	std::cout << "FPGA status: " << status;
 
 	/* check for any FPGA error */
 	if (NiFpga_IsNotError(status))
@@ -67,48 +47,6 @@ int main(void)
 
 		if (NiFpga_IsNotError(status))
 		{
-			/*
-			int i;
-			for(i=0; i<size; i=i+1){
-			data[i] = i*5000;
-
-
-			for(i=0; i<size; i=i+1){
-			printf("%i\n",data[i]);
-			}
-			getchar();
-			*/
-
-			/*
-			int16_t ahex = (int16_t) (10.0/10*_I16_MAX);
-			printf("%i\n", ahex);
-			char hex[16];
-			sprintf(hex, "%x", ((ahex + (1 << Abits)) % (1 << Abits)));
-			puts(hex);
-			getchar();
-			*/
-
-
-			/*
-			int16_t val = -32769;
-			char hex[16];
-			sprintf(hex, "%x", ((val + (1 << 16)) % (1 << 16)) );
-			puts(hex);
-			getchar();*/
-
-
-			/*the AO reads a I16, specifically
-			0x7FFF = 32767
-			0xFFFF = -1
-			0x8000 = -32768*/
-
-			/*
-			printf("%i\n", VOUT(10));
-			getchar();*/
-
-		
-
-
 			NiFpga_MergeStatus(&status, NiFpga_WriteU16(session, NiFpga_FPGA_ControlU16_InitialWaittick, initialWait));
 
 			/*DELAY. Sync AO and DO by delaying DO*/
@@ -120,13 +58,13 @@ int main(void)
 			const size_t sizeFifo = 2;
 			/*AO1 FIFO*/
 			size_t r1; //empty elements remaining
-			int32_t *AOfifo = malloc(sizeFifo * sizeof(int32_t));
+			uint32_t *AOfifo = new uint32_t[sizeFifo];
 			for (int i = 0; i < sizeFifo; i++) { //initialize the array
 				AOfifo[i] = 0;
 			}
 
 			int16_t t0 = 0x0000;
-			int16_t Vout0 = VOUT(-10);
+			int16_t Vout0 = VOUT(10);
 			int16_t t1 = 0x00A0;
 			int16_t Vout1 = VOUT(0);
 
@@ -138,9 +76,9 @@ int main(void)
 			//AOfifo[5] = 0x00A00000;
 			NiFpga_MergeStatus(&status, NiFpga_WriteFifoU32(session, NiFpga_FPGA_HostToTargetFifoU32_A0FIFO, AOfifo, sizeFifo, timeout, &r1)); //send the AO data
 
-			/*DO FIFO*/
+																																			   /*DO FIFO*/
 			size_t r2; //empty elements remaining
-			uint32_t *DOfifo = malloc(sizeFifo * sizeof(int32_t));
+			uint32_t *DOfifo = new uint32_t[sizeFifo];
 
 			for (int i = 0; i < sizeFifo; i++) { //initialize the array
 				DOfifo[i] = 0;
@@ -154,7 +92,6 @@ int main(void)
 			NiFpga_MergeStatus(&status, NiFpga_WriteFifoU32(session, NiFpga_FPGA_HostToTargetFifoU32_DOFIFO, DOfifo, sizeFifo, timeout, &r2)); //send the DO data
 
 
-
 			/* run the FPGA application.*/
 			NiFpga_MergeStatus(&status, NiFpga_Run(session, 0));
 
@@ -162,15 +99,15 @@ int main(void)
 			NiFpga_Bool start = 1;
 			NiFpga_MergeStatus(&status, NiFpga_WriteBool(session, NiFpga_FPGA_ControlBool_start, start));
 
-			
+
 
 			/* close the session. THIS TURNS OFF THE OUTPUT OF THE FPGA */
-			delay(1); //temp hack to let the FPGA finish before shutting it down
+			delay(2); //temp hack to let the FPGA finish before shutting it down
 			NiFpga_MergeStatus(&status, NiFpga_Close(session, 0));
 
 			/*cleanup*/
-			free(AOfifo);
-			free(DOfifo);
+			delete[] AOfifo;
+			delete[] DOfifo;
 
 		}
 
@@ -185,4 +122,41 @@ int main(void)
 }
 
 
+/*
+int i;
+for(i=0; i<size; i=i+1){
+data[i] = i*5000;
 
+
+for(i=0; i<size; i=i+1){
+printf("%i\n",data[i]);
+}
+getchar();
+*/
+
+/*
+int16_t ahex = (int16_t) (10.0/10*_I16_MAX);
+printf("%i\n", ahex);
+char hex[16];
+sprintf(hex, "%x", ((ahex + (1 << Abits)) % (1 << Abits)));
+puts(hex);
+getchar();
+*/
+
+
+/*
+int16_t val = -32769;
+char hex[16];
+sprintf(hex, "%x", ((val + (1 << 16)) % (1 << 16)) );
+puts(hex);
+getchar();*/
+
+
+/*the AO reads a I16, specifically
+0x7FFF = 32767
+0xFFFF = -1
+0x8000 = -32768*/
+
+/*
+printf("%i\n", VOUT(10));
+getchar();*/

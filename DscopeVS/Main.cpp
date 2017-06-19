dffdb6afec8d0621a014b7b9f4e2d91ae8b00cd1
David Chen <chen@mpi-cbg.de>
2017-06-19 20:02:45 +0200

    Added: linear ramp (needs debugging)

--- DscopeVS/Main.cpp
+++ DscopeVS/Main.cpp
@@ -3,10 +3,12 @@
 //#include <ansi_c.h>
 //#include <limits.h>
 //#include <math.h>
-#include <time.h>
+//#include <time.h>
+#include "windows.h"		//the stages use this lib. also Sleep
 #include "NiFpga_FPGA.h"
 #include <iostream>
 #include "main.h"
+//#include <concrt.h> 	//Concurrency::wait(2000);
 
 
 
@@ -14,20 +16,35 @@
 static const char* const Bitfile = "D:\\OwnCloud\\Codes\\Dscope\\DscopeVS\\LabView\\FPGA Bitfiles\\" NiFpga_FPGA_Bitfile;
 
 
-
-/*Halt the program (in seconds)*/
-void delay(double dly) {
-	/* save start time */
-	const time_t start = time(NULL);
+void linearRamp(uint32_t *aa, tt_t ti, int16_t Vi, tt_t tf, int16_t Vf)
+{
+	int nPoints = (int) ((1.*tf-ti) / AO_dt);
 	
-	time_t current;
-	do {
-		/* get current time */
-		time(&current);
+	tt_t *tPoints = new tt_t[nPoints + 1];
+	//uint16_t *vPoints = new uint16_t[nPoints+1];
+	for (int ii = 0; ii < nPoints; ii++)
+	{
+		tPoints[ii] = ti + ii * AO_dt;
+		//vPoints[ii] = VOUT( Vi + (1.*Vf-Vi)/(tf-ti)*(tPoints[ii] - ti)   );
 
-		/* break loop when the requested number of seconds have elapsed */
-	} while (difftime(current, start) < dly);
+		aa[ii] = AO_dt << Abits | (LSBmask & (  VOUT(  Vi + (1.*Vf - Vi) / (tf - ti)*(tPoints[ii] - ti)  )  )  );
+	}
+	//tPoints[nPoints] = tf;
+	//vPoints[nPoints] = VOUT(Vf);
+	aa[nPoints] = AO_dt << Abits | (LSBmask & VOUT(Vf));
+
+	
+	//std::cout << nPoints;
+	/*
+	for (int ii = 0; ii <= nPoints; ii++)
+		std::cout << tPoints[ii] << "\n";
+	getchar();
+	*/
+	
 }
+
+
+
 
 
 int main()
@@ -35,7 +52,7 @@ int main()
 	/* must be called before any other FPGA calls */
 	NiFpga_Status status = NiFpga_Initialize();
 
-	std::cout << "FPGA status: " << status;
+	std::cout << "FPGA status: " << status << "\n";
 
 	/* check for any FPGA error */
 	if (NiFpga_IsNotError(status))
@@ -55,42 +72,45 @@ int main()
 			NiFpga_MergeStatus(&status, NiFpga_WriteU16(session, NiFpga_FPGA_ControlU16_DOCalibratetick, calibrateDOtiming));
 
 
-			const size_t sizeFifo = 2;
+			const size_t sizeFifo = 23+1;
+
 			/*AO1 FIFO*/
-			size_t r1; //empty elements remaining
+			size_t rAO1; //empty elements remaining
 			uint32_t *AOfifo = new uint32_t[sizeFifo];
 			for (int i = 0; i < sizeFifo; i++) { //initialize the array
 				AOfifo[i] = 0;
 			}
 
-			int16_t t0 = 0x0000;
+			tt_t t0 = 0*us;
 			int16_t Vout0 = VOUT(10);
-			int16_t t1 = 0x00A0;
+			tt_t t1 = 160*us;
 			int16_t Vout1 = VOUT(0);
 
-			AOfifo[0] = (t0 << Abits) | (LSBmask & Vout0);
-			AOfifo[1] = (t1 << Abits) | (LSBmask & Vout1);
+			//AOfifo[0] = (t0 << Abits) | (LSBmask & Vout0);
+			//AOfifo[1] = (t1 << Abits) | (LSBmask & Vout1);
 			//AOfifo[2] = 0x00A03FFF;
 			//AOfifo[3] = 0x00A00000;
 			//AOfifo[4] = 0x00A00000;
 			//AOfifo[5] = 0x00A00000;
-			NiFpga_MergeStatus(&status, NiFpga_WriteFifoU32(session, NiFpga_FPGA_HostToTargetFifoU32_A0FIFO, AOfifo, sizeFifo, timeout, &r1)); //send the AO data
+			linearRamp(AOfifo, 0*us, 0, 1150*us, 10);
+			
+			NiFpga_MergeStatus(&status, NiFpga_WriteFifoU32(session, NiFpga_FPGA_HostToTargetFifoU32_A0FIFO, AOfifo, sizeFifo, timeout, &rAO1)); //send the AO data
 
-																																			   /*DO FIFO*/
-			size_t r2; //empty elements remaining
+
+			/*DO FIFO*/
+			size_t rDO1; //empty elements remaining
 			uint32_t *DOfifo = new uint32_t[sizeFifo];
-
 			for (int i = 0; i < sizeFifo; i++) { //initialize the array
 				DOfifo[i] = 0;
 			}
+
 			DOfifo[0] = 0x00000001;
 			DOfifo[1] = 0x00A00000;
 			//DOfifo[2] = 0x00A00001;
 			//DOfifo[3] = 0x00A00001;
 			//DOfifo[4] = 0x00A00000;
 			//DOfifo[5] = 0x00A00000;
-			NiFpga_MergeStatus(&status, NiFpga_WriteFifoU32(session, NiFpga_FPGA_HostToTargetFifoU32_DOFIFO, DOfifo, sizeFifo, timeout, &r2)); //send the DO data
-
+			NiFpga_MergeStatus(&status, NiFpga_WriteFifoU32(session, NiFpga_FPGA_HostToTargetFifoU32_DOFIFO, DOfifo, sizeFifo, timeout, &rDO1)); //send the DO data
 
 			/* run the FPGA application.*/
 			NiFpga_MergeStatus(&status, NiFpga_Run(session, 0));
@@ -100,15 +120,14 @@ int main()
 			NiFpga_MergeStatus(&status, NiFpga_WriteBool(session, NiFpga_FPGA_ControlBool_start, start));
 
 
-
 			/* close the session. THIS TURNS OFF THE OUTPUT OF THE FPGA */
-			delay(2); //temp hack to let the FPGA finish before shutting it down
+			Sleep(2000);//temp hack to let the FPGA finish before shutting it down
+		
 			NiFpga_MergeStatus(&status, NiFpga_Close(session, 0));
 
 			/*cleanup*/
 			delete[] AOfifo;
 			delete[] DOfifo;
-
 		}
 
 
@@ -135,10 +154,10 @@ getchar();
 */
 
 /*
-int16_t ahex = (int16_t) (10.0/10*_I16_MAX);
-printf("%i\n", ahex);
+int16_t test = 160;
+printf("%i\n", test);
 char hex[16];
-sprintf(hex, "%x", ((ahex + (1 << Abits)) % (1 << Abits)));
+sprintf(hex, "%x", ((test + (1 << Abits)) % (1 << Abits)));
 puts(hex);
 getchar();
 */
--- DscopeVS/main.h
+++ DscopeVS/main.h
@@ -3,22 +3,25 @@
 
 #define tickPerUs 40				// Number of ticks in 1 us
 #define us 1
+#define ms 1000
 #define tick 1
 #define LSBmask 0x0000FFFF			// Select the 16 least significant bits
 #define Abits 16					// Number of bits of the analog output
+#define AO_dt 50					// Analog output time increament
+typedef uint16_t tt_t;				// Time type
 
-
-#define VOUT(x) (int16_t)(1.0*x / 10 *_I16_MAX)
+#define VOUT(x) (int16_t)(1.0*x / 10 *_I16_MAX)		//converts voltage +-10 to hex
+#define TIME(x) (int16_t)(1.0*x / 10 *_I16_MAX)		//convert time to hex
 
 /*FIFO variables*/
 static const uint32_t timeout = 1; // in ms. 
 
 
 /*DELAY. Sync AO and DO by delaying DO*/
-static const uint16_t DOfifoDelayTick = 76 * tick; //relative delay between AO and DO
+static const uint16_t DOfifoDelayTick = (76+80) * tick; //relative delay between AO and DO
 static const uint16_t calibrateAOtiming = 40 * tick; //fine-tune the AO timing
 static const uint16_t calibrateDOtiming = 5 * tick; //fine-tune the DO timing
-static const uint16_t initialWait = 100 * tick; //Initial wait-time before the entire sequence starts
+static const uint16_t initialWait = 500 * tick; //Initial wait-time before the entire sequence starts
 
 
 

d8fac9abcd0cb7ffeb4aad55f76d2a8815f9ddc0
David Chen <davidchenlee@gmail.com>
2018-04-15 20:10:10 +0200

    LV: add internal FIFO to deepen to DMA FIFO
    2 32-bit FIFOs, 16k each
    ~ 90% of the RAM used. No chance of adding more memory to the FIFOs

--- Maincode/MCMain.cpp
+++ Maincode/MCMain.cpp
@@ -41,7 +41,7 @@ int main()
 
 			//Closes the session to the FPGA. The FPGA resets (Re-downloads the FPGA bitstream to the target, the outputs go to zero)
 			//unless either another session is still open or you use the NiFpga_CloseAttribute_NoResetIfLastSession attribute.
-			NiFpga_MergeStatus(&status, NiFpga_Close(session, 1)); //0 resets, 1 does not reset
+			NiFpga_MergeStatus(&status, NiFpga_Close(session, 0)); //0 resets, 1 does not reset
 		}
 
 		//Reset the FPGA

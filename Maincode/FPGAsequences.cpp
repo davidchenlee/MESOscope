bd9a31091f54ef28fd3df87f5bbdb4b1cffad63a
David Chen <davidchenlee@gmail.com>
2018-04-18 09:48:02 +0200

    LV: bundle the feedback nodes in the 'start stop' subvi

--- Maincode/FPGAsequences.cpp
+++ Maincode/FPGAsequences.cpp
@@ -59,7 +59,7 @@ U32QV Scan2D()
 	U32Q linearRampSegment0 = generateLinearRamp(GalvoStep,25 * ms, -GalvoAmplitude_volt, GalvoAmplitude_volt);	//Ramp up the galvo from -GalvoAmplitude_volt to GalvoAmplitude_volt
 	
 	VectorOfQueues[ABUF0] = linearRampSegment0;
-	VectorOfQueues[ABUF0].push(generateSingleAnalogOut(4 * us, -GalvoAmplitude_volt));								//Set the galvo back to -GalvoAmplitude_volt
+	//VectorOfQueues[ABUF0].push(generateSingleAnalogOut(4 * us, -GalvoAmplitude_volt));								//Set the galvo back to -GalvoAmplitude_volt
 	
 	/*//debugger
 	VectorOfQueues[ABUF0].push(generateSingleAnalogOut(4 * us, 0));

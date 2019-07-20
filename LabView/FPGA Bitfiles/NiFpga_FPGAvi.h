6ea5a2f1010f8fa57584cd6d3de87b53b7ceb717
David Chen <davidchenlee@gmail.com>
2019-07-20 11:35:25 +0200

    LV: clean up. VS: recompile all the projects (config VS for openCL)

--- LabView/FPGA Bitfiles/NiFpga_FPGAvi.h
+++ LabView/FPGA Bitfiles/NiFpga_FPGAvi.h
@@ -24,7 +24,7 @@
 /**
  * The signature of the FPGA bitfile.
  */
-static const char* const NiFpga_FPGAvi_Signature = "486368FFE20D18EF0A7A2B797AE84459";
+static const char* const NiFpga_FPGAvi_Signature = "79449A3DCCDD3F791755B03FB9041643";
 
 typedef enum
 {
